b37133e713b0260ced7d63d2d2fe24127ecd8aee
Rudy Casilla <94497674+rpccode@users.noreply.github.com>
2022-08-16 01:27:55 +0200

    f

---
+++ config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', '@cluster0.lsect.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'guitarla_strapi'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'root'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', 'admin'),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f16c1e41-748c-402d-a768-f5a51955f7ee'
+};
